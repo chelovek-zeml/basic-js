ffa319b942ed4f9007629d90da248044d07b5e11
chelovek-zeml <stranger37561@gmail.com>
2022-10-11 15:01:35 +0300

    feat: add tasks sort-by-height, sum-digits

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+ let array = [];
+
+ for(let i of arr) {
+  if(i !== -1) {
+    array.push(i);
+  }
+ }
+array.sort(function(a, b) {
+  return a - b;
+});
+
+for(let i = 0; i < arr.length; i++) {
+  if(arr[i] === -1) {
+    array.splice(i, 0, -1);
+  }
+}
+return array;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(date.getMonth() < 3) {
+    return 'winter';
+  }
+  if(date.getMonth() < 6 && date.getMonth() > 2) {
+    return 'spring';
+  }
+  if(date.getMonth() < 9 && date.getMonth() > 5) {
+    return 'summer';
+  }
+  if(date.getMonth() < 12 && date.getMonth() > 8) {
+   if(date.getMonth() === 11 && date.getDate() > 30) {
+    return 'winter';
+   }
+    return 'fall';
+  }
 }
 
 module.exports = {
