6850f8b4755e98f30be804652184be027e8b0814
chelovek-zeml <stranger37561@gmail.com>
2022-10-16 15:28:56 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,7 +16,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(str, options) {
- 
+  if(!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+  if(!options.separator) {
+    options.separator = "+";
+  }
+  if(!String(options.addition) || String(options.addition) === 'undefined') {
+    options.addition = '';
+  }
+  if(!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+  if(!options.additionSeparator) {
+    options.additionSeparator = '|';
+  }
+  
+
+  let arrTemp = [];
+   let arrRes = [];
+
+   for(let i = 0; i < options.additionRepeatTimes; i++) {
+    arrTemp.push(String(options.addition))
+   }
+   for(let i = 0; i < options.repeatTimes; i++) {
+    arrRes.push(str + arrTemp.join(options.additionSeparator));
+   }
+   return arrRes.join(options.separator);
 }
 
 module.exports = {
